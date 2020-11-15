b35299fb577df8150dcea1930c58579a7af93efd
papatomatoe <hole_in_the_wall@list.ru>
2020-11-15 14:08:40 +0300

    add: save in localStorage

--- js/script.js
+++ js/script.js
@@ -2,6 +2,8 @@ const messageList = document.querySelector('.messages');
 const messageTemplate = document.querySelector('#msg').content;
 const form = document.querySelector('form');
 
+const savedMessages = JSON.parse(localStorage.getItem('savedMessages')) || {};
+
 const getDataFromForm = () => {
   const elements = form.elements;
   const data = {};
@@ -12,13 +14,13 @@ const getDataFromForm = () => {
   return data;
 }
 
-const addMessage = (name, text) => {
+const addMessage = (name, text, savedDate) => {
   const messageCopy = messageTemplate.cloneNode(true);
   const username = messageCopy.querySelector('.message__username');
   const message = messageCopy.querySelector('.message__text');
   const delBtn = messageCopy.querySelector('.message__del')
   const date = messageCopy.querySelector('.message__date');
-  const currentDate = new Date();
+  const currentDate = new Date(savedDate);
 
   username.innerText = name;
   message.innerText = text;
@@ -26,7 +28,11 @@ const addMessage = (name, text) => {
   date.innerText = currentDate.toLocaleString();
   date.setAttribute('datetime', currentDate);
 
-  delBtn.addEventListener('click', evt => evt.target.parentNode.remove());
+  delBtn.addEventListener('click', evt => {
+    delete savedMessages[name]
+    localStorage.setItem('savedMessages', JSON.stringify(savedMessages));
+    evt.target.parentNode.remove();
+  });
 
   messageList.append(messageCopy);
 }
@@ -34,6 +40,17 @@ const addMessage = (name, text) => {
 form.addEventListener('submit', (evt) => {
   evt.preventDefault();
   const { username, usermessage } = getDataFromForm();
-  addMessage(username, usermessage);
+  const date = Date.now();
+
+  savedMessages[username] = {};
+  savedMessages[username].message = usermessage;
+  savedMessages[username].date = date;
+
+
+  localStorage.setItem('savedMessages', JSON.stringify(savedMessages))
+  addMessage(username, usermessage, date);
   form.reset();
 })
+
+
+Object.entries(savedMessages).sort(([, b], [, d]) => b.date - d.date).forEach(([name, { message, date }]) => addMessage(name, message, date));
